b0b54b3780c95ff74d94f374572078a496500a83
Max Goldman <maxg@mit.edu>
2015-09-14 16:02:40 -0400

    Greeting in Java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    
+    public static void main(String[] args) {
+        printGreeting();
+    }
+    
+    public static void printGreeting() {
+        System.out.println("Hello, version control!");
+    }
+}
