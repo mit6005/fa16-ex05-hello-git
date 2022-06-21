214e553a6930c4d34319058acf5c0a68fa0f33d9
Igor Narvaev-Ivanov <inarvaev@gehtsoft.com>
2022-06-21 17:43:24 +0600

    Rename method

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
     
     public static void main(String[] args) {
-        printGreeting();
+        printGreeting1();
     }
     
-    public static void printGreeting() {
+    public static void printGreeting1() {
         System.out.println("Hello, version control!");
     }
 }
